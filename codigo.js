4582b882e4d860caf7a00903882392f29267d647
Gonzalo Sanchez <gonzaezeesanchez@gmail.com>
2023-06-16 20:00:26 -0300

    terminado

--- codigo.js
+++ codigo.js
@@ -41,3 +41,14 @@
 // 		}
 
 
+function cambiarFondo(color) {
+    document.querySelector('body').style.background =color;
+}
+
+
+// const texto = document.getElementById('texto');
+//     const contar = document.getElementById('contar');
+    
+//     texto.addEventListener('keyup',function(){
+//     contar.innerText= texto.value.length;
+//     })
